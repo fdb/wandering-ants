7e2683c57479ed3f227a7d257893f6cafa01cc29
Frederik De Bleser <frederik@debleser.be>
2016-05-27 10:03:12 +0200

    Optimized build.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-clang -g -o ants -Ivendor/nanovg `pkg-config --cflags --libs sdl2` -framework OpenGL src/ants.c vendor/nanovg/nanovg.c
+clang -O3 -g -o ants -Ivendor/nanovg `pkg-config --cflags --libs sdl2` -framework OpenGL src/ants.c vendor/nanovg/nanovg.c
 
